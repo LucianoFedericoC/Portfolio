67f424184114bc9a0d3dfb4f96cbc46d1f89bfdd
Luciano Carducci <carduccilucianof@gmail.com>
2024-09-03 22:07:36 -0300

    add head tag with viewport

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
-import logo from '../assets/SVG/logo.svg'
+import logo from '../assets/SVG/logo.svg';
 import './globals.css';
 import 'dotenv/config';
 import Footer from '@/components/navigation/footer';
@@ -9,7 +9,7 @@ import { ThemeProvider } from './theme-provider.tsx';
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-	icons: {icon: '/logo.svg'},
+	icons: { icon: '/logo.svg' },
 	title: 'Luciano Carducci',
 };
 
@@ -19,14 +19,21 @@ export default function RootLayout({
 	children: React.ReactNode;
 }>) {
 	return (
-	    <html lang="en">
-      <body
-        className={`${inter.className}`}
-      >
-        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-          <main>{children}</main>
-        </ThemeProvider>
-      </body>
-    </html>
+		<html lang='en'>
+			<head>
+				<meta
+					name='viewport'
+					content='width=device-width, initial-scale=1.0'
+				/>
+			</head>
+			<body className={`${inter.className}`}>
+				<ThemeProvider
+					attribute='class'
+					defaultTheme='system'
+					enableSystem>
+					<main>{children}</main>
+				</ThemeProvider>
+			</body>
+		</html>
 	);
 }
